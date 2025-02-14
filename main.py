467dbc035f8d60aa930f3ce8b56a50d819b689e1
yymato <070913a@gmail.com>
2025-02-14 16:59:16 +0300

    Коммит для задачи полный поиск исправление

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
+from io import BytesIO
 
 import requests
+from PIL import Image
 
 from modules.drawer import drawer
 from modules.size_func import selection_size
@@ -9,7 +11,6 @@ from modules.size_func import selection_size
 # python search.py Москва, ул. Ак. Королева, 12
 # Тогда запрос к геокодеру формируется следующим образом:
 toponym_to_find = " ".join(sys.argv[1:])
-
 geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 
 geocoder_params = {
@@ -25,7 +26,6 @@ if not response:
 
 # Преобразуем ответ в json-объект
 json_response = response.json()
-print(json_response)
 # Получаем первый топоним из ответа геокодера.
 toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
 # Координаты центра топонима:
@@ -40,8 +40,8 @@ apikey = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
 map_params = {
     "ll": ",".join([toponym_longitude, toponym_lattitude]),
     "spn": selection_size(toponym),
+    "pt": f'{toponym_longitude},{toponym_lattitude},comma',
     "apikey": apikey,
-
 }
 
 map_api_server = "https://static-maps.yandex.ru/v1"
--- modules/drawer.py
+++ modules/drawer.py
@@ -1,8 +1,8 @@
 from io import BytesIO
-from PIL.Image import Image
+from PIL import Image
 
 
-def drawer(content)
+def drawer(content):
     im = BytesIO(content)
     opened_image = Image.open(im)
     opened_image.show()  # Создадим картинку и тут же ее покажем встроенным просмотрщиком операционной системы
