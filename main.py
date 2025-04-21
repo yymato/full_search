274b9b4fb061d2ebd4df19131fda8417a49955e5
yymato <070913a@gmail.com>
2025-04-21 19:02:47 +0300

    Задача Угадай-ка город

--- main.py
+++ main.py
@@ -1,59 +1,97 @@
 import sys
-from io import BytesIO
-from operator import length_hint
-
+import random
 import requests
-from PIL import Image
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton, QVBoxLayout, QWidget
 
-from modules.drawer import drawer
-from modules.lonlat_dist import lonlat_dist
-from modules.size_func import selection_size
+# Список городов
+CITIES = [
+    "Москва", "Санкт-Петербург", "Новосибирск", "Екатеринбург", "Казань",
+    "Нижний Новгород", "Челябинск", "Самара", "Ростов-на-Дону", "Уфа"
+]
 
-# Пусть наше приложение предполагает запуск:
-# python search.py Москва, ул. Ак. Королева, 12
-# Тогда запрос к геокодеру формируется следующим образом:
-toponym_to_find = " ".join(sys.argv[1:])
-geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+YANDEX_STATIC_MAPS_API = "https://static-maps.yandex.ru/1.x/"
+YANDEX_GEOCODE_API = "http://geocode-maps.yandex.ru/1.x/"
+YANDEX_GEOCODE_KEY = "8013b162-6b42-4997-9691-77b7074026e0"
 
-geocoder_params = {
-    "apikey": "8013b162-6b42-4997-9691-77b7074026e0",
-    "geocode": toponym_to_find,
-    "format": "json"}
 
-response = requests.get(geocoder_api_server, params=geocoder_params)
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("крестики нолики")
+        self.setFixedSize(600, 450)
 
-if not response:
-    # обработка ошибочной ситуации
-    pass
+        self.map_label = QLabel(self)
+        self.map_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.next_button = QPushButton("Следующий город")
+        self.next_button.clicked.connect(self.show_city)
+        layout = QVBoxLayout()
+        layout.addWidget(self.map_label)
+        layout.addWidget(self.next_button)
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
 
-# Преобразуем ответ в json-объект
-json_response = response.json()
-# Получаем первый топоним из ответа геокодера.
-toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-# Координаты центра топонима:
-toponym_coodrinates = toponym["Point"]["pos"]
-# Долгота и широта:
-toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
-self_point = f'{toponym_longitude},{toponym_lattitude}'
+        self.rem_cities = CITIES.copy()
+        random.shuffle(self.rem_cities)
 
-geocoder_params = {
-    "apikey": "8013b162-6b42-4997-9691-77b7074026e0",
-    "geocode": self_point,
-    "kind": 'district',
-    "format": "json"}
+        self.show_city()
 
-response = requests.get(geocoder_api_server, params=geocoder_params)
+    def show_city(self):
+        if not self.rem_cities:
+            self.rem_cities = CITIES.copy()
+            random.shuffle(self.rem_cities)
 
-if not response:
-    # обработка ошибочной ситуации
-    pass
+        city = self.rem_cities.pop()
+        coords = self.get_city_coords(city)
+        if coords:
+            self.display_map(coords)
 
-# Преобразуем ответ в json-объект
-json_response = response.json()
-# Получаем первый топоним из ответа геокодера.
-toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-# Координаты центра топонима:
-for i in toponym['metaDataProperty']['GeocoderMetaData']["Address"]["Components"]:
-    if 'district' in i.values():
-        print(i["name"])
-        break
+    @staticmethod
+    def get_city_coords(city):
+        params = {
+            "apikey": YANDEX_GEOCODE_KEY,
+            "geocode": city,
+            "format": "json"
+        }
+        response = requests.get(YANDEX_GEOCODE_API, params=params)
+        if not response:
+            return None
+        json_data = response.json()
+        pos = json_data["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]["pos"]
+        lon, lat = pos.split()
+        return float(lon), float(lat)
+
+
+    def display_map(self, coords):
+        lon, lat = coords
+        map_params = {
+            "ll": f"{lon},{lat}",
+            "z": 12,
+            "l": "sat"
+        }
+
+        response = requests.get(YANDEX_STATIC_MAPS_API, params=map_params)
+        print(response.reason)
+        print(response.url)
+        if not response:
+            return
+
+        with open("city.jpg", "wb") as f:
+            f.write(response.content)
+
+        pixmap = QPixmap("city.jpg")
+        self.map_label.setPixmap(pixmap)
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    sys.excepthook = except_hook
+    game_window = MainWindow()
+    game_window.show()
+    sys.exit(app.exec())
